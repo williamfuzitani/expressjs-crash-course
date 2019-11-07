34d4552e152f227387af5edaea8d4d773dba2c41
William <william@imac-de-william.local>
2019-11-07 16:45:06 -0300

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Express Crash Course
+
+# Express JS Crash Course
+- https://www.youtube.com/watch?v=L72fhGm1tfE
---
+++ routes/api/members.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const uuid = require("uuid");
+const router = express.Router();
+const members = require("../../Members");
+
+// Get all members
+router.get("/", (req, res) => {
+    res.json(members);
+});
+
+// Get single member
+router.get("/:id", (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+        res.json(
+            members.filter(member => member.id === parseInt(req.params.id))
+        );
+    } else {
+        res.status(400).json({ msg: `No member with id: ${req.params.id}` });
+    }
+});
+
+// Create member
+router.post("/", (req, res) => {
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: "active"
+    };
+
+    if (!newMember.name || !newMember.email) {
+        return res.status(400).json({ msg: "Please include a name and email" });
+    }
+
+    members.push(newMember);
+    res.json(members);
+});
+
+module.exports = router;
